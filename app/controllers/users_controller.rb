9164c7982979687f3842ceb20bbefcabcad5cc75
Maurice <mcadenhe@gmail.com>
2014-03-08 22:45:20 -0500

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   	def create
   		@user = User.new(user_params)    # Not the final implementation!
     	if @user.save
-    		flash[:success] = "Welcome to the Sample App!"
+        sign_in @user
+    		flash[:success] = "Welcome to Promptku!"
       		redirect_to @user
     	else
     		render 'new'
