bb7950d766d38427fd31b6448220726a6b0c4286
Maurice <mcadenhe@gmail.com>
2014-05-08 15:27:34 -0400

    Bootstrap bs not working in heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
-//= require bootstrap
 //= require_tree .
 //= require jquery.infinitescroll
 //= require manual-trigger
