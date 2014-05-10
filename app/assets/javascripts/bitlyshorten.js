85a9873b8fb8cb565a402b71570830bb8b9ed529
Maurice <mcadenhe@gmail.com>
2014-05-10 06:25:50 -0400

    Add ssl to new bitly links

--- app/assets/javascripts/bitlyshorten.js
+++ app/assets/javascripts/bitlyshorten.js
@@ -1,7 +1,7 @@
 function get_short_url(long_url, login, api_key, func)
 	{
 	    $.getJSON(
-	        "http://api.bitly.com/v3/shorten?callback=?", 
+	        "https://api-ssl.bitly.com/v3/shorten?callback=?", 
 	        { 
 	            "format": "json",
 	            "apiKey": api_key,
