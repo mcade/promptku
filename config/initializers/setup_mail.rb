6ddefbdc9babb3dd87a306e19152dd76e37b0505
Maurice <mcadenhe@gmail.com>
2014-05-16 20:39:47 -0400

    fix mailer link

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -8,4 +8,4 @@ ActionMailer::Base.smtp_settings = {
   :enable_starttls_auto => true
 }
 
-ActionMailer::Base.default_url_options[:host] = 'https://www.promptku.com/'
+ActionMailer::Base.default_url_options[:host] = 'https://www.promptku.com'
